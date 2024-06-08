58ccb643f540f1d6b6987322f8bafb50df0119dd
Marc-Antoine Ruel <maruel@gmail.com>
2024-06-08 18:04:24 -0400

    Update esphome, add log.sh

---
+++ log.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Copyright 2023 Marc-Antoine Ruel. All rights reserved.
+# Use of this source code is governed under the Apache License, Version 2.0
+# that can be found in the LICENSE file.
+
+# Capture log output from a device using log_debug.yaml
+
+set -eu
+
+cd "$(dirname $0)"
+cd ..
+if [ ! -f .venv/bin/activate ]; then
+  echo "run setup.sh"
+  exit 1
+fi
+
+source .venv/bin/activate
+esphome logs $1
